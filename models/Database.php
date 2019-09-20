333d866c89e404be36cf96456fd7881c1d15e236
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-20 11:40:02 +0200

    Mise à jour du fichier class Database.php

--- models/Database.php
+++ models/Database.php
@@ -1,25 +1,17 @@
-<?php
+<?php 
 
-//Connexion à la base de données
+// Test de remplacement numéro 1
 
-//A placer dans le Modèle : cette partie gère les données de votre site. Son rôle est d'aller récupérer les informations « brutes » dans la base de données, de les organiser et de les assembler pour qu'elles puissent ensuite être traitées par le contrôleur. On y trouve donc entre autres les requêtes SQL.
+class Database
+{
 
-// ajout d'un try catch die pour afficher un message d'erreur lorsque l'on n'est pas connecté à une bdd existante
-
-//On essaie de se connecter à la base de données dans le bloc  try . Si tout va bien, on continue (on va dans le "Code après"). Si en revanche il y a un souci lors de la connexion (à l'intérieur du new PDO), alors on récupère l'erreur dans le bloc catch
-
-try{
-    $bdd = new PDO('mysql:host=localhost;dbname=bdd_mvc;charset=utf8', 'root', '');
+	protected function connect_bdd()
+    {
     
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
-    //gère le type d'erreurs lors de la connexion à la BDD - l'option ERRMODE_SILENT est l'option par défaut qui assigne simplement les messages d'erreurs (ERRMODE_SILENT - voir la documentation MySQL)
-    
-}catch(PDOException $e){
-    
-    die($e->getMessage());
-    //la fonction getMessage permet d'indiquer un message d'erreur lorsque l'on n'est pas connecté à une bdd existante
-    
+    	$db = new \PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+
+    	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    	return $db;
+    }
 }
-
-
-?>
