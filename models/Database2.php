8d2e7a7ebe48cbfc46939370aae89739c0172521
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-21 12:35:24 +0200

    Create Database2.php

---
+++ models/Database2.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Database2
+{
+
+	protected function db_connect()
+    {
+    
+    	$db = new \PDO('mysql:host=localhost;dbname=bootmvc30;charset=utf8', 'root', '');
+
+    	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    	return $db;
+    }
+}
