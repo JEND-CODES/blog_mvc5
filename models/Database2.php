81878f1ccb0d5f0dab5d530fba1683adcd897c64
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-23 21:38:16 +0200

    Update Database2.php

--- models/Database2.php
+++ models/Database2.php
@@ -3,10 +3,10 @@
 class Database2
 {
 
-	protected function bdd_connected()
+	protected function connectDB()
     {
     
-    	$db = new \PDO('mysql:host=localhost;dbname=bootmvc30;charset=utf8', 'root', '');
+    	$db = new PDO('mysql:host=localhost;dbname=bootmvc30;charset=utf8', 'root', '');
 
     	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
