d88a9aaa97bdfb02798a656b7475045b7e371ccc
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-25 16:54:39 +0200

    New Class POO Login

--- controllers/controllerLogin.php
+++ controllers/controllerLogin.php
@@ -1,52 +1,66 @@
 <?php
-session_start();
 
-// Si la session est en cours..
-if(!empty($_SESSION['connect']))
-    
-    // Autorisation d'accès au Back Office
-    header('Location:'.URL.'connect');
+// POO class -> Connexion au Back Office
 
-//Voir demo https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
-
-if(!empty($_POST))
+class ControllerLogin
 {
-    extract($_POST);
-    $errors = array();
+    private $admin_log;
     
-    // Sécurisation contre injections
-    
-    $user = htmlentities($user);
-    
-    $password = htmlentities($password);
-
-    if(empty($user))
-        array_push($errors, 'Identifiant manquant');
-
-    if(empty($password))
-        array_push($errors, 'Mot de passe manquant');
-
-    
-    if(!empty($user) && !empty($password))
-    { 
-        $repositoryConnect = new RepositoryConnect($bdd);
+    public function __construct(){
         
-        
-        if(!$repositoryConnect->checkUser($user, $password))
-            array_push($errors, 'Mauvais identifiants');
-        else
+        $this->admin_log = new RepositoryConnect();
+    }
+    
+    public function Login()
+    {
+
+        session_start();
+        // Si la session est en cours..
+        if(!empty($_SESSION['connect']))
+
+        // Autorisation d'accès au Back Office
+        header('Location:'.URL.'connect');
+
+        // Voir demo https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
+
+        if(!empty($_POST))
         {
-           
-            //Activation de $_SESSION 
-            //https://www.php.net/manual/fr/reserved.variables.session.php
-            //https://www.php.net/manual/fr/ref.session.php
-            $_SESSION['connect'] = $user;
-            
-            header('Location:'.URL.'connect');
+            extract($_POST);
+            $errors = array();
+
+            // Sécurisation contre injections
+
+            $user = htmlentities($user);
+
+            $password = htmlentities($password);
+
+            if(empty($user))
+                array_push($errors, 'Identifiant manquant');
+
+            if(empty($password))
+                array_push($errors, 'Mot de passe manquant');
+
+
+            if(!empty($user) && !empty($password))
+            { 
+                
+                if(!$this->admin_log->checkUser($user, $password))
+                    array_push($errors, 'Mauvais identifiants');
+                else
+                {
+
+                    //Activation de $_SESSION 
+                    //https://www.php.net/manual/fr/reserved.variables.session.php
+                    //https://www.php.net/manual/fr/ref.session.php
+                    
+                    $_SESSION['connect'] = $user;
+
+                    header('Location:'.URL.'connect');
+                }
+            }
         }
+
+        require_once('views/viewLogin.php');
+            
     }
 }
-
-
-
-require_once('views/viewLogin.php');
