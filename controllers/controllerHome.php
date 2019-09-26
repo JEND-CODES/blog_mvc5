8a792523533536c200bfe108d92d9af37263bea6
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-26 21:26:58 +0200

    Programmation orientée objet

--- controllers/controllerHome.php
+++ controllers/controllerHome.php
@@ -1,82 +1,95 @@
 <?php
 
-//Contrôleur : cette partie gère la logique du code qui prend des décisions. C'est en quelque sorte l'intermédiaire entre le modèle et la vue : le contrôleur va demander au modèle les données, les analyser, prendre des décisions et renvoyer le texte à afficher à la vue. Le contrôleur contient exclusivement du PHP. C'est notamment lui qui détermine si le visiteur a le droit de voir la page ou non (gestion des droits d'accès).
+// POO class
 
-//Mise à jour du contrôleur - Maintenant que nous avons créé des classes, il nous faut créer des objets à partir d'elles. cf.tuto openC - https://openclassrooms.com/fr/courses/4670706-adoptez-une-architecture-mvc-en-php/4735671-passage-du-modele-en-objet
-
-// A placer dans le dossier controller/controllerAccueil.php
-
-//Instanciation de l'objet création de l'objet $repositoryArticle et récupération des articles avec création de l'objet $articles
-//Créer et manipuler un objet Créer un objet Nous allons voir comment créer un objet, c'est-à-dire que nous allons utiliser notre classe afin qu'elle nous fournisse un objet.
-
-//L'auto-chargement de classes - Pour une question d'organisation, il vaut mieux créer un fichier par classe.
-
-//le constructeur de la classe appelle chaque setter pour assigner les valeurs qu'on lui a données aux attributs correspondants
-
-$repositoryChapter = new RepositoryChapter($bdd);
-
-$chapters1 = $repositoryChapter->selectChapters1();
-
-$repositoryMessage = new RepositoryMessage($bdd);
-
-$messages = $repositoryMessage->selectMessages();
-
-// Images du slideshow
-$repositoryBackground = new RepositoryBackground($bdd);
-
-$backgrounds = $repositoryBackground->selectBackgrounds();
-
-if(!empty($_POST))
+class ControllerHome
 {
-    extract($_POST);
-    $errors = array();
+    private $posts;
+    private $images;
+    private $writings;
     
-    // Empêcher les attaques XSS. Utiliser la fonction plus approprié htmlentities()
-    // En savoir plus sur les failles XSS -> https://openclassrooms.com/fr/courses/2091901-protegez-vous-efficacement-contre-les-failles-web/2680167-la-faille-xss
-    //XSS (plus officiellement appelée Cross-Site Scripting) est une faille permettant l'injection de code HTML ou JavaScript dans des variables mal protégées
-    
-    $email = htmlentities($email);
-    
-    $infoname = htmlentities($infoname);
-    
-    $content = htmlentities($content);
+    // Déclaration d'attributs/propriétés privées -> La visibilité d'une propriété, d'une méthode ou (à partir de PHP 7.1.0) une constante peut être définie en préfixant sa déclaration avec un mot-clé : public, protected, ou private. Les éléments déclarés comme publics sont accessibles partout. L'accès aux éléments protégés est limité à la classe elle-même, ainsi qu'aux classes qui en héritent et parente. L'accès aux éléments privés est uniquement réservé à la classe qui les a définis (Cf. https://www.php.net/manual/fr/language.oop5.visibility.php)
 
-    if(empty($email))
-        array_push($errors, 'Entrez votre Email');
-   
-    //condition pour avoir un format mail valide, et avoir obligatoirement le symbole @ dans l'email cf.https://stackoverflow.com/questions/5855811/how-to-validate-an-email-in-php //https://www.php.net/manual/en/function.preg-match.php // https://www.guru99.com/php-regular-expressions.html
-    if (!preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/", $email)) {
-       array_push($errors, 'Format de mail invalide');
-    }
-    
-    if(empty($infoname))
-        array_push($errors, 'Nom manquant');
-    
-    if(!empty($infoname) && strlen($infoname)>100)
-        array_push($errors, 'Nom trop long');
-
-    if(empty($content))
-        array_push($errors, 'Message manquant');
-    
-    //limite du nombre de caractères pour l'envoi du message en page accueil
-    if(!empty($content) && strlen($content)>280)
-        array_push($errors, 'Message trop long');
-
-    if(count($errors) == 0)
-    { 
-        $plus = new Message(array('email'=>$email, 'infoname'=>$infoname, 'content'=>$content));
+    public function __construct()
+    {
+        $this->posts = new RepositoryChapter;
+        // Permet de remplacer l'instanciation classique dans ce format : $repositoryChapter = new RepositoryChapter();
         
-        $repositoryMessage->insertMessage($plus);
-
-        $more = 'Message envoyé';
-
-        unset($email);
-        unset($infoname);
-        unset($content);
+        $this->images = new RepositoryBackground;
+        
+        $this->writings = new RepositoryMessage;
+        
+        // PHP permet aux développeurs de déclarer des constructeurs pour les classes. Les classes qui possèdent une méthode constructeur appellent cette méthode à chaque création d'une nouvelle instance de l'objet, ce qui est intéressant pour toutes les initialisations dont l'objet a besoin avant d'être utilisé (Cf. https://www.php.net/manual/fr/language.oop5.decon.php)
+        
     }
+    
+    public function Home()
+    {
+
+        // Permet d'appeler la function du RepositoryChapter en suivant la méthode "__construct"
+        
+        $chapters = $this->posts->selectChaptersDesc();
+        
+        // Idem pour les images du slideshow
+        $backgrounds = $this->images->selectBackgrounds();
+        
+        // Gestion des erreurs et de la validation du formulaire de messagerie en page d'accueil
+        if(!empty($_POST))
+        {
+            extract($_POST);
+            $errors = array();
+
+            // Empêcher les attaques XSS. Utiliser la fonction plus appropriée : htmlentities()
+            // En savoir plus sur les failles XSS -> https://openclassrooms.com/fr/courses/2091901-protegez-vous-efficacement-contre-les-failles-web/2680167-la-faille-xss
+            // XSS (plus officiellement appelée Cross-Site Scripting) est une faille permettant l'injection de code HTML ou JavaScript dans des variables mal protégées
+
+            $email = htmlentities($email);
+
+            $infoname = htmlentities($infoname);
+
+            $content = htmlentities($content);
+
+            if(empty($email))
+                array_push($errors, 'Entrez votre Email');
+
+            // Condition pour avoir un format mail valide, et avoir obligatoirement le symbole @ dans l'email cf.https://stackoverflow.com/questions/5855811/how-to-validate-an-email-in-php //https://www.php.net/manual/en/function.preg-match.php // https://www.guru99.com/php-regular-expressions.html
+            
+            if (!preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/", $email)) {
+               array_push($errors, 'Format de mail invalide');
+            }
+
+            if(empty($infoname))
+                array_push($errors, 'Nom manquant');
+
+            if(!empty($infoname) && strlen($infoname)>100)
+                array_push($errors, 'Nom trop long');
+
+            if(empty($content))
+                array_push($errors, 'Message manquant');
+
+            // Limite du nombre de caractères pour l'envoi du message
+            
+            if(!empty($content) && strlen($content)>280)
+                array_push($errors, 'Message trop long');
+
+            if(count($errors) == 0)
+            { 
+                $plus = new Message(array('email'=>$email, 'infoname'=>$infoname, 'content'=>$content));
+
+                //Appel de l'attribut privé "writings" de la classe ControllerHome, puis de la fonction du RepositoryMessage
+                
+                $this->writings->insertMessage($plus);
+
+                $more = 'Message envoyé';
+
+                unset($email);
+                unset($infoname);
+                unset($content);
+            }
+        }
+
+        require_once('views/viewHome.php');
+
+    }
+  
 }
-
-
-require_once('views/viewHome.php');
-
-
