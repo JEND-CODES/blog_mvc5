8fce135fc7a3744effa5aaa3033441b66e02cd33
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-27 22:14:54 +0200

    POO class ControllerPassword

--- controllers/controllerPassword.php
+++ controllers/controllerPassword.php
@@ -1,51 +1,68 @@
 <?php
-session_start();
 
-if(empty($_SESSION['connect']))
-    header('Location:'.URL.'login');
+// POO class -> Changement de mot de passe en Back Office
 
-$repositoryConnect = new RepositoryConnect($bdd);
-
-$connect = $repositoryConnect->selectUser($_SESSION['connect']);
-
-if(!empty($_POST))
+class ControllerPassword
 {
-    extract($_POST);
-    $errors = array();
+    private $new_pass;
     
-    $password = htmlentities($password);
-    
-    $password2 = htmlentities($password2);
-    
-    $checkpwd2 = htmlentities($checkpwd2);
-
-    if(empty($password))
-        array_push($errors, 'Entrez le mot de passe actuel');
-    
-    if(!empty($password) && sha1($password) != $connect->password())
-        array_push($errors, 'Mot de passe actuel inexact');
-    
-    if(empty($password2))
-        array_push($errors, 'Nouveau mot de passe manquant');
-
-    if(!empty($password2) && strlen($password2)>15)
-        array_push($errors, 'Nouveau mot de passe trop long');
-
-    if($checkpwd2 != $password2)
-        array_push($errors, 'Champ de confirmation inexact');
-
-    if(count($errors) == 0)
-    { 
-        $connect->setPassword(sha1($password2));
+    public function __construct()
+    {
         
-        $repositoryConnect->updatePassword($connect);
+        $this->new_pass = new RepositoryConnect();
+    }
+    
+    public function Password()
+    {
 
-        $nouveau = 'Mot de passe modifié';
+        session_start();
 
-        unset($password);
-        unset($password2);
-        unset($checkpwd2);
+        if(empty($_SESSION['connect']))
+        header('Location:'.URL.'login');
+        
+        $connect = $this->new_pass->selectUser($_SESSION['connect']);
+
+        if(!empty($_POST))
+        {
+            extract($_POST);
+            $errors = array();
+
+            $password = htmlentities($password);
+
+            $password2 = htmlentities($password2);
+
+            $checkpwd2 = htmlentities($checkpwd2);
+
+            if(empty($password))
+                array_push($errors, 'Entrez le mot de passe actuel');
+
+            if(!empty($password) && sha1($password) != $connect->password())
+                array_push($errors, 'Mot de passe actuel inexact');
+
+            if(empty($password2))
+                array_push($errors, 'Nouveau mot de passe manquant');
+
+            if(!empty($password2) && strlen($password2)>15)
+                array_push($errors, 'Nouveau mot de passe trop long');
+
+            if($checkpwd2 != $password2)
+                array_push($errors, 'Champ de confirmation inexact');
+
+            if(count($errors) == 0)
+            { 
+                $connect->setPassword(sha1($password2));
+
+                $this->new_pass->updatePassword($connect);
+
+                $nouveau = 'Mot de passe modifié';
+
+                unset($password);
+                unset($password2);
+                unset($checkpwd2);
+            }
+        }
+
+        require_once('views/viewPassword.php');
+            
     }
 }
-
-require_once('views/viewPassword.php');
