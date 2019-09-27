c3ce29e5ad269abc6d0a0d609ca656d1d2b261b8
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-27 22:34:33 +0200

    Extension de classe Database

--- models/RepositoryBackground.php
+++ models/RepositoryBackground.php
@@ -1,20 +1,15 @@
 <?php
-class RepositoryBackground
+
+class RepositoryBackground extends Database
 {
-    private $_bdd;
-    
-    public function __construct($bdd)
-    {
-        $this->setBdd($bdd);
-    }
     
     public function selectBackgrounds()
     {  
         $backgrounds = array();
         
-        $req = $this->_bdd->query('SELECT id, title, content, DATE_FORMAT(date, \'%d/%m/%Y à %Hh %imin %ss\') AS backgroundDate FROM backgrounds ORDER BY id DESC');
+        $req = $this->connectDB()->query('SELECT id, title, content, DATE_FORMAT(date, \'%d/%m/%Y à %Hh %imin %ss\') AS backgroundDate FROM backgrounds ORDER BY id DESC');
         $req->execute();
-        while($data = $req->fetch(PDO::FETCH_ASSOC))
+        while($data = $req->fetch())
         {
             $backgrounds[] = new Background($data);
         }
@@ -24,12 +19,12 @@ class RepositoryBackground
    
     public function selectBackground($id)
     {
-        $req = $this->_bdd->prepare('SELECT id, title, content, DATE_FORMAT(date, \'%d/%m/%Y à %Hh %imin %ss\') AS backgroundDate FROM backgrounds WHERE id = ?');
+        $req = $this->connectDB()->prepare('SELECT * FROM backgrounds WHERE id = ?');
         $req->execute(array($id));
         
         if($req->rowCount() == 1)
         {
-            $data = $req->fetch(PDO::FETCH_ASSOC);
+            $data = $req->fetch();
             return new Background($data);   
         }
         
@@ -38,22 +33,18 @@ class RepositoryBackground
     
     public function insertBackground($ajouter)
     {
-        $req = $this->_bdd->prepare('INSERT INTO backgrounds (title, content, date) VALUES(?, ?, NOW())');
+        $req = $this->connectDB()->prepare('INSERT INTO backgrounds (title, content, date) VALUES(?, ?, NOW())');
         $req->execute(array($ajouter->title(), $ajouter->content()));
         $req->closeCursor();    
     }
    
     public function deleteBackground($retirer)
     {
-        $req = $this->_bdd->prepare('DELETE FROM backgrounds WHERE id = ?');
+        $req = $this->connectDB()->prepare('DELETE FROM backgrounds WHERE id = ?');
         $req->execute(array($retirer));
         $req->closeCursor();    
     }
     
-    public function setBdd(PDO $bdd)
-    {
-        $this->_bdd = $bdd;
-    }
+    
 }
 
-?>
