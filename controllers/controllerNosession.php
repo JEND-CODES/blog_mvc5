63c60178a43545df3077a867d1048f70f5ca4b9d
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-27 22:14:18 +0200

    Nouvelle classe ControllerNosession

--- controllers/controllerNosession.php
+++ controllers/controllerNosession.php
@@ -1,13 +1,21 @@
 <?php
-//session_start — Démarre une nouvelle session ou reprend une session existante
-//https://www.php.net/manual/fr/function.session-start.php
-session_start();
 
-unset($_SESSION['connect']);
+class ControllerNosession
+{
+    
+    public function Nosession()
+    {
+  
+        //session_start — Démarre une nouvelle session ou reprend une session existante
+        //https://www.php.net/manual/fr/function.session-start.php
+        session_start();
 
-//https://www.php.net/manual/fr/function.session-destroy.php
-session_destroy();
+        unset($_SESSION['connect']);
 
-header('Location:'.URL.'login');
+        //https://www.php.net/manual/fr/function.session-destroy.php
+        session_destroy();
 
-?>
+        header('Location:'.URL.'login');
+
+   }
+}
