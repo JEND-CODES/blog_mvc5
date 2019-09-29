089bd539ec42b79bc372c5b5ac5733e2e0207819
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-29 23:05:42 +0200

    Variables PHP pour remplacements de balises HTML

---
+++ content/tags.php
@@ -0,0 +1,31 @@
+<?php
+
+    // Variables correspondantes aux balises HTML et aux caractères spéciaux pour leurs décodages dans viewHome :
+
+    // -> Voir le fichier script.js pour d'autres remplacements de chaînes de caractères -> méthode .replace() jQuery.. 
+
+        $sym0 = '&euml;';
+        $sym1 = '&amp;';
+        $sym2 = '&iuml;';
+        $sym3 = '&auml;';
+        $sym4 = '&ouml;';
+        $sym5 = '&pound;';
+        $sym6 = '&ugrave;';
+        $sym7 = '&micro;';
+        $sym8 = '&sect;';
+        $acc1 = '&eacute;';  
+        $acc2 = '&ecirc;'; 
+        $acc3 = '&agrave;'; 
+        $br = '<br />'; 
+        $div = '<div>';
+        $div2 = '</div>';
+        $em = '</em>';
+        $em2 = '<em>';  
+        $p = '<p>';
+        $p2 = '</p>';  
+        $endTag = '</a>';
+        $nbs = '&nbsp;';   
+        $stg = '<strong>';
+        $stg2 = '</strong>';
+        $span = '<span>';
+        $span2 = '</span>';
