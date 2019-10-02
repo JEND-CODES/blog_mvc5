43e2c304f394fb207e69e617619c6c8a8cfca582
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-10-02 18:59:52 +0200

    Réglages Html réguliers

--- views/viewBiographie.php
+++ views/viewBiographie.php
@@ -6,8 +6,8 @@ $nav_title = "Biographie de Jean Forteroche";
 
 </header>
 
-<br>
-<br>
+<br />
+<br />
 
 <section id="biographie">
 
@@ -29,31 +29,32 @@ $nav_title = "Biographie de Jean Forteroche";
 
                     <div class="row">
 
-                        <span class="card-title activator grey-text text-darken-4">
+                        <div class="card-title activator grey-text text-darken-4">
 
                             <p class="activator">Grand voyageur, Jean Forteroche est la preuve que le polar n'a pas de limite géographique. Il y a deux ans, après un séjour à Stockholm, il publiait Onitsha et nous emmenait en motoneige en pleine toundra, au coeur du pays Sami...</p>
 
                             <i class="material-icons right">more_vert</i>
 
-                        </span>
+                        </div>
 
                     </div>
                 </div>
 
                 <div class="card-reveal">
 
-                    <span class="card-title grey-text text-darken-4">
+                    <div class="card-title grey-text text-darken-4">
 
                         <p id="random-color">Biographie</p>
 
                         <p>Utque proeliorum periti rectores primo catervas densas opponunt et fortes, deinde leves armaturas, post iaculatores ultimasque subsidiales acies, si fors adegerit, iuvaturas, ita praepositis urbanae familiae suspensae digerentibus sollicite...</p>
 
                         <i class="material-icons right">close</i>
-                    </span>
+                    </div>
 
                 </div>
             </div>
         </div>
+    </div>
 
 </section>
 
