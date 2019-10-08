41335f11506ae661c154763ae009b9875e212a45
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-10-08 11:37:30 +0200

    Extension de la classe Common

--- models/Chapter.php
+++ models/Chapter.php
@@ -1,5 +1,5 @@
 <?php
-class Chapter
+class Chapter extends Common
 {
     
     //Création d'attributs - Pour déclarer des attributs, on va donc les écrire entre les accolades, les uns à la suite des autres, en faisant précéder leurs noms du mot-clé private //Vous pouvez constater que chaque attribut est précédé d'un underscore (« _ »). Ceci est une notation qu'il est préférable de respecter (il s'agit de la notation PEAR) qui dit que chaque nom d'élément privé doit être précédé d'un underscore
@@ -14,35 +14,14 @@ class Chapter
     
     // constructeur -> Comme son nom l'indique, le constructeur sert à construire l'objet.
     
-    //Le constructeur d'une classe a pour rôle principal d'initialiser l'objet en cours de création, c'est-à-dire d'initialiser la valeur des attributs (soit en assignant directement des valeurs spécifiques, soit en appelant diverses méthodes).
-    public function __construct(array $data)
-    {
-        $this->hydrate($data);
-    }
-    
-    // méthode hydrate - L'hydratation consiste à transformer le contenu d'une base de données en objets // Voir tuto OpenC https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1666289-manipulation-de-donnees-stockees
-    public function hydrate(array $data)
-    {
-        foreach($data as $key => $value)
-            //Il est possible de changer la visibilité d'une méthode ainsi que son nom grâce au mot-clé as.
-        {
-            
-            // On récupère le nom du setter correspondant à l'attribut
-            $method = 'set'.ucfirst($key);
-            
-            // Si le setter correspondant existe
-            // On appelle le setter
-            if(method_exists($this, $method))
-                $this->$method($value);
-        }
-    }
-    
-    //Pour lire ou modifier un attribut, on utilise des accesseurs (getters) et des mutateurs (setters)
+    // Le constructeur d'une classe a pour rôle principal d'initialiser l'objet en cours de création, c'est-à-dire d'initialiser la valeur des attributs (soit en assignant directement des valeurs spécifiques, soit en appelant diverses méthodes).
+
+    // Pour lire ou modifier un attribut, on utilise des accesseurs (getters) et des mutateurs (setters)
     
     // getters
-    //lorsque les attributs des objets sont inaccessibles (private et encapsulés dans une classe) -> Il faut créer des getters pour pouvoir les lire, et des setters pour pouvoir modifier leurs valeurs.
+    // lorsque les attributs des objets sont inaccessibles (private et encapsulés dans une classe) -> Il faut créer des getters pour pouvoir les lire, et des setters pour pouvoir modifier leurs valeurs.
     
-    //Appeler les méthodes de l'objet - Pour appeler une méthode d'un objet, il va falloir utiliser un opérateur : il s'agit de l'opérateur " -> " 
+    // Appeler les méthodes de l'objet - Pour appeler une méthode d'un objet, il va falloir utiliser un opérateur : il s'agit de l'opérateur " -> " 
     public function getId()
     {
         return $this->_id;    
@@ -76,7 +55,7 @@ class Chapter
         return $this->_refreshDate;
     } 
     
-    //fonction setId vérifie si l'ID est bien supérieure à zéro (ce qui doit forcément être le cas dans la table SQL)
+    // fonction setId vérifie si l'ID est bien supérieure à zéro (ce qui doit forcément être le cas dans la table SQL)
     public function setId($id)
     {
         $id = (int) $id;
@@ -84,7 +63,7 @@ class Chapter
         if($id > 0)
             $this->_id = $id;
     }
-    //fonction setTitle vérifie s'il s'agit bien d'une chaîne de caractères
+    // fonction setTitle vérifie s'il s'agit bien d'une chaîne de caractères
     public function setTitle($title)
     {
         if(is_string($title))
@@ -112,7 +91,7 @@ class Chapter
     public function setZerolink($zerolink)
     {
         if(is_string($zerolink))
-            //Pour affichage image par défault des chapitres
+            // Pour affichage image par défault des chapitres
             $this->_zerolink = $zerolink;
     }
     public function setChapterDate($chapterDate)
