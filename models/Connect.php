10dcd973aa6635dfc761d06ca1c2cbd21943e09c
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-10-08 11:38:47 +0200

    Extension de la classe Common

--- models/Connect.php
+++ models/Connect.php
@@ -1,28 +1,10 @@
 <?php
-class Connect
+class Connect extends Common
 {
     private $_id;
     private $_user;
     private $_password;
-    
-    
-    public function __construct(array $data)
-    {
-        $this->hydrate($data);
-    }
-    
-    
-    public function hydrate(array $data)
-    {
-        foreach($data as $key => $value)
-        {
-            $method = 'set'.ucfirst($key);
-            
-            if(method_exists($this, $method))
-                $this->$method($value);
-        }
-    }
-    
+
     
     public function getId()
     {
